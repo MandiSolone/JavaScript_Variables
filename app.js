3d2a0c58d0ed05f70809271d139ebfc3d10da18f
[Mandi Solone] <[mandisolone@gmail.com]>
2024-06-26 16:35:55 -0400

    added Javascript

--- app.js
+++ app.js
@@ -4,7 +4,40 @@ console.log(
 );
 
 // Exercise 1
-
+let lastName;
+var age; 
 // Exercise 2
+const firstName = "Mandi";
+lastName = "Solone";
+age = 35; 
+
+console.log ("Hi, my name is " + firstName + " " + lastName +" & I am " + age + ".");
+
 
 // Exercise 3
+let language = "JavaScript"; 
+let createdYear = 1995; 
+let isCaseSensitive = true; 
+
+console.log(`${language} was first release in ${createdYear}.`);
+console.log(isCaseSensitive);
+
+// Exercise 4
+let product = "T-shirt";
+let price = 19.99; 
+let isonSale = false; 
+let stock = 0; 
+let inStock = false; 
+let selectedSize = "M";
+console.log(
+  `${product} in size ${selectedSize} is $${price} with ${stock} in stock.`
+  );
+
+// Exercise 5
+let title = "Name of the Wind"; 
+let author = "Patrick Rothfuss"; 
+let pageCount = 722; 
+let bookmark = 456; 
+let hasRead = true;  
+
+console.log(`"${title}" was written by ${author} and is ${pageCount} pages long and your last bookmarked page was ${bookmark}`); 
